5d70bccb879bba5eb4fa6ec99a38dac4418aa32f
t-east <azmtomya.a127@gmail.com>
2022-03-21 15:48:47 +0900

    cors対策

--- src/drivers/router/router.go
+++ src/drivers/router/router.go
@@ -12,8 +12,22 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
+func allowOptionsMiddleware(w http.ResponseWriter, r *http.Request) error {
+	if r.Method == "OPTIONS" {
+		w.WriteHeader(http.StatusOK)
+		return nil
+	}
+	return nil
+}
+
 func ServerHandlerPublic(w http.ResponseWriter, r *http.Request) {
+	param, _, _ := mocks.CreateParamMock()
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+    w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+    w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	allowOptionsMiddleware(w, r)
 	var head string
+	_, r.URL.Path = core.ShiftPath(r.URL.Path)
 	head, r.URL.Path = core.ShiftPath(r.URL.Path)
 	switch head {
 	case "content":
