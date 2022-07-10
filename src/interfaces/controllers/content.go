ae987afdaba65b0e3234ca47269c44ecf75ca1c9
t-east <azmtomya.a127@gmail.com>
2022-07-10 20:31:06 +0900

    モック作成

--- src/interfaces/controllers/content.go
+++ src/interfaces/controllers/content.go
@@ -1,11 +1,12 @@
 package controllers
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	"os"
 	"user/src/domains/entities"
-	"user/src/interfaces/contracts"
-	"user/src/interfaces/crypt"
-	"user/src/usecases/interactor"
 	"user/src/usecases/port"
 
 	"github.com/labstack/echo/v4"
@@ -29,62 +30,90 @@ func LoadContentController(param *entities.Param) *ContentController {
 	}
 }
 
+type MetaRes struct {
+	Receipt entities.Content `json:"receipt"`
+	PrivKey []byte           `json:"priv_key"`
+	PubKey  []byte           `json:"pub_key"`
+}
+
 func (cc *ContentController) MetaGen(c echo.Context) error {
 	req := &entities.ContentIn{}
 	if err := c.Bind(req); err != nil {
 		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 	}
-	content := &entities.ContentIn{
-		Content: req.Content,
-		PrivKey: req.PrivKey,
-		Address: req.Address,
-	}
-	crypt := crypt.NewContentCrypt(cc.Param)
-	contract := contracts.NewContentContracts()
-	inputPort := interactor.NewContentInputPort(
-		crypt,
-		contract,
-	)
-	receipt, err := inputPort.MetaGen(content)
+	// crypt := crypt.NewContentCrypt(cc.Param)
+	// contract := contracts.NewContentContracts()
+	// inputPort := interactor.NewContentInputPort(
+	// 	crypt,
+	// 	contract,
+	// )
+	// receipt, privKey, pubKey, err := inputPort.MetaGen(content)
+	// if err != nil {
+	// 	return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	// }
+	raw, err := ioutil.ReadFile("./content.json")
 	if err != nil {
-		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+		fmt.Println(err.Error())
+		os.Exit(1)
 	}
-	return c.JSON(http.StatusCreated, receipt)
+
+	var fc entities.Content
+
+	json.Unmarshal(raw, &fc)
+	return c.JSON(http.StatusCreated, MetaRes{
+		Receipt: entities.Content{
+			Content:  req.Content,
+			MetaData: fc.MetaData,
+		},
+		PrivKey: req.PrivKey,
+	})
 }
 
 type SetKeyReq struct {
-	PubKey     string `json:"pub_key"`
-	EthPrivKey string `json:"eth_priv_key"`
+	PubKey []byte `json:"pub_key"`
 }
 
 func (cc *ContentController) SetKey(c echo.Context) error {
-	req := &SetKeyReq{}
-	if err := c.Bind(req); err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
-	}
-	crypt := crypt.NewContentCrypt(cc.Param)
-	contract := contracts.NewContentContracts()
-	inputPort := interactor.NewContentInputPort(
-		crypt,
-		contract,
-	)
-	err := inputPort.SetKey(req.PubKey, req.EthPrivKey)
-	if err != nil {
-		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
-	}
+	// req := &SetKeyReq{}
+	// if err := c.Bind(req); err != nil {
+	// 	return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	// }
+	// crypt := crypt.NewContentCrypt(cc.Param)
+	// contract := contracts.NewContentContracts()
+	// inputPort := interactor.NewContentInputPort(
+	// 	crypt,
+	// 	contract,
+	// )
+	// err := inputPort.SetKey(req.PubKey)
+	// if err != nil {
+	// 	return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	// }
 	return c.NoContent(http.StatusCreated)
 }
 
 func (cc *ContentController) GetLog(c echo.Context) error {
-	crypt := crypt.NewContentCrypt(cc.Param)
-	contract := contracts.NewContentContracts()
-	inputPort := interactor.NewContentInputPort(
-		crypt,
-		contract,
-	)
-	content, err := inputPort.GetLog()
+	// crypt := crypt.NewContentCrypt(cc.Param)
+	// contract := contracts.NewContentContracts()
+	// inputPort := interactor.NewContentInputPort(
+	// 	crypt,
+	// 	contract,
+	// )
+	// content, err := inputPort.GetLog()
+	// if err != nil {
+	// 	return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	// }
+	raw, err := ioutil.ReadFile("./log.json")
 	if err != nil {
-		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+		fmt.Println(err.Error())
+		os.Exit(1)
 	}
-	return c.JSON(http.StatusOK, content)
+
+	var fc entities.Log
+
+	json.Unmarshal(raw, &fc)
+	logs := []entities.Log{
+		fc,
+		fc,
+	}
+	return c.JSON(http.StatusOK, logs)
 }
--- src/main.go
+++ src/main.go
@@ -2,16 +2,24 @@ package main
 
 import (
 	"log"
-	"user/src/drivers/ethereum"
+	"user/src/core"
 	router "user/src/drivers/router"
 	"user/src/interfaces/controllers"
+
+	"github.com/joho/godotenv"
 )
 
 func realMain() {
-	param, err := ethereum.GetParam()
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(".env not found")
+	}
+	// param, err := ethereum.GetParam()
+	param, _, err := core.CreateParamMock()
 	if err != nil {
 		log.Fatal(err)
 	}
+	log.Print(param)
 	cc := *controllers.LoadContentController(param)
 
 	e := router.NewServer(cc)
