01d1e77ee47e50a6b2a6562e148e93516ad88baa
t-east <azmtomya.a127@gmail.com>
2022-10-24 11:29:30 +0900

    fix secret

--- src/drivers/ethereum/secret.go
+++ src/drivers/ethereum/secret.go
@@ -3,8 +3,5 @@ package ethereum
 import "os"
 
 var EthPrivKey = os.Getenv("USER_PRIVATE_KEY")
-var ethContentAddress = os.Getenv("CONTENT_ADDRESS")
-var ethAuditAddress = os.Getenv("AUDIT_ADDRESS")
-var ethParamAddress = os.Getenv("PARAM_ADDRESS")
-var ethPubKeyAddress = os.Getenv("PUBKEY_ADDRESS")
+var ethContractAddress = os.Getenv("CONTRACT_ADDRESS")
 var ganaHost = os.Getenv("GANA_HOST")
